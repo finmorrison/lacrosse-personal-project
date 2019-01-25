b244019f8e33803071d91cb4bf207a19a22ffd85
Fin  Morrison <finmorrison@Fins-MacBook-Pro.local>
2019-01-25 12:38:07 -0700

    new

--- server/index.js
+++ server/index.js
@@ -20,10 +20,10 @@ app.use(
     saveUninitialized: false
   })
 );
-const path = require('path')
-app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+// const path = require('path')
+// app.get('*', (req, res)=>{
+//   res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
 app.use( express.static( `${__dirname}/../build` ) );
 
 massive(CONNECTION_STRING).then(db => {
